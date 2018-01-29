ddcd158b5012fa3376f93a6b8670a810c7d72f09
Juanlu Romero <juanlu_21@hotmail.com>
2018-01-29 16:42:12 +0100

    añadido bootstrap 4 y modificadas vistas para usarlo

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -5,29 +5,29 @@
         {{ $films->links() }}
     </div>
     @forelse($films as $film)
-        <div class="row">
+        <div class="row my-4">
             <div class="col-md-4">
                 <a href="/films/show/{{ $film['id'] }}">
                     <img src="{{$film['cover']}}" alt="{{$film['name']}} cover" class="img-fluid img-thumbnail">
-                    <div class="centered-rating"><h1 class="display-3">{{ $film['rating'] }}</h1></div>
+                    <div class="centered-rating"><h1 class="display-4">{{ $film['rating'] }}</h1></div>
                 </a>
             </div>
-            <div class="col-8">
+            <div class="col-md-8">
                 <div class="row">
                     <div class="row">
                         <h1 class="display-4 col">{{ $film['name'] }}</h1>
                     </div>
                     <div class="row">
                         <div class="col-md-2">
-                            <div class="row">Views: {{ $film['views_counted'] }}</div>
-                            <div class="row">Reviews: {{ $film['reviews_counted'] }}</div>
+                            <div class="row offset-1">Views: {{ $film['views_counted'] }}</div>
+                            <div class="row offset-1">Reviews: {{ $film['reviews_counted'] }}</div>
                         </div>
-                        <div class="col-6">{{ $film['synopsis'] }}</div>
+                        <div class="col-md-6 mx-2">{{ $film['synopsis'] }}</div>
                     </div>
                 </div>
             </div>
         </div>
     @empty
-        <h2 class="display-4">No hay datos a mostrar</h2>
+        <h4>No hay datos a mostrar</h4>
     @endforelse
 @endsection
