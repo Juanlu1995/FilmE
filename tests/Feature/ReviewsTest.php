ece6acb7e7d3d4ada0251f9803a2de98a265d848
Juanlu Romero <juanlu_21@hotmail.com>
2018-03-09 17:34:25 +0100

    faltan algunas rutas

--- tests/Feature/ReviewsTest.php
+++ tests/Feature/ReviewsTest.php
@@ -109,8 +109,76 @@ class ReviewsTest extends TestCase
     {
         $user = $this->createUser();
         $film = $this->createFilm($user);
-        $response = $this->actingAs($user)->get('/reviews/create/'.$film->id);
+        $response = $this->actingAs($user)->get('/reviews/create/' . $film->id);
         $response->assertStatus(200);
         $response->assertSee($film->cover);
+    }
+
+    /**
+     * Probamos la ruta de creación de una review
+     *
+     * POST /reviews
+     */
+    public function testPostCreateNewReview()
+    {
+        $user = $this->createUser();
+        $film = $this->createFilm($user);
+
+        $review = Factory(Review::class)->create(['user_id' => $user->id, 'film_id' => $film->id]);
+
+        $response = $this->actingAs($user)->post('/reviews', [
+            'title' => $review->title,
+            'content' => $review->content,
+            'rating' => $review->rating,
+            'film' => $film->id
+        ]);
+
+        $response->assertStatus(302);
+
+        $this->assertDatabaseHas('reviews', [
+            'title' => $review->title
+        ]);
+    }
+
+
+    /**
+     * Probamos la ruta de edición de una review
+     *
+     * PATCH /reviews/{review}
+     */
+    public function testPostUpdateReview()
+    {
+        $user = $this->createUser();
+
+        $review = $this->createReview($user, null);
+
+        $newContent = $review->content . 'holaquetal';
+
+        $response = $this->actingAs($user)->patch('/reviews/' . $review->id, [
+            'title' => $review->title,
+            'content' => $newContent,
+            'rating' => $review->rating,
+            'id' => $review->id
+        ]);
+
+        $response->assertStatus(302);
+        $this->assertDatabaseHas('reviews', [
+            'id' => $review->id,
+            'content' => $newContent
+        ]);
+    }
+
+    /**
+     * Probamos acceder a la ruta de edicion de una review
+     *
+     * GET /reviews/film/{film}
+     */
+    public function testShowEditReview()
+    {
+        $user = $this->createUser();
+        $review = $this->createReview($user,null);
+        $response = $this->actingAs($user)->get('/reviews/'.$review->id.'/edit');
+        $response->assertStatus(200);
+        $response->assertSee($review->title);
     }
 }
